9f4585db60140b2d9b70ba2614b7d9f10e317761
Alerson Rigo <alersonrigo@Alersons-MacBook-Pro.local>
2024-03-19 12:59:48 -0300

    refactoring

--- scripts/init-localstack.sh
+++ scripts/init-localstack.sh
@@ -6,4 +6,4 @@ aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name my-queue-
 aws --endpoint-url=http://localhost:4566 sqs list-queues --region us-east-1
 
 # to send message
-aws --endpoint-url=http://localhost:4566 sqs send-message --queue-url "http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/my-queue-name" --message-body "Eniando mensagem na fila SQS" --region us-east-1
+aws --endpoint-url=http://localhost:4566 sqs send-message --queue-url "http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/my-queue-name" --message-body "Enviando mensagem na fila SQS" --region us-east-1
